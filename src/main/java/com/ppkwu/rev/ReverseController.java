8d0c0f8bc330174a64f42f8479f39f1e38c12528
Piotr Kupis <piotr.kupis42@gmail.com>
2021-10-14 12:47:37 +0200

    Add GET method responsible for reversing string

--- src/main/java/com/ppkwu/rev/ReverseController.java
+++ src/main/java/com/ppkwu/rev/ReverseController.java
@@ -1,5 +1,8 @@
 package com.ppkwu.rev;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -8,4 +11,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class ReverseController {
 
+    /**
+     * Method responsible for reversing string.
+     *
+     * @param value string to be reversed
+     * @return object of type {@link ResponseEntity} with reversed string
+     */
+    @GetMapping("reverse/{value}")
+    public ResponseEntity<String> reverseString(@PathVariable("value") String value) {
+        StringBuilder stringBuilder = new StringBuilder(value);
+        stringBuilder.reverse();
+        return ResponseEntity.ok(stringBuilder.toString());
+    }
 }
