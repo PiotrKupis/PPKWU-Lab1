195e5a7292248552edc96c655beca737a9d681e8
Piotr Kupis <piotr.kupis42@gmail.com>
2021-10-14 12:33:35 +0200

    Add project structure

---
+++ src/test/java/com/ppkwu/rev/RevApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ppkwu.rev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RevApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
