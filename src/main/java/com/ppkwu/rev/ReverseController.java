16cfa9c9e14876de838429addd92305e92629b6c
Piotr Kupis <piotr.kupis42@gmail.com>
2021-10-14 12:35:55 +0200

    Add controller responsible for reversing strings

---
+++ src/main/java/com/ppkwu/rev/ReverseController.java
@@ -0,0 +1,11 @@
+package com.ppkwu.rev;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Controller responsible for reversing strings.
+ */
+@RestController
+public class ReverseController {
+
+}
